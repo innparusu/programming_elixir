0b1ae9b258a9e51abf5273196bcf878cdb7c8143
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-09-19 00:43:49 +0900

    Add Issues.CLI module and test

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -16,5 +16,16 @@ defmodule Issues.CLI do
 
   Otherwise it is a github user name, project name, and (optionally)
   the number of entries to format.
+  Return a tuple of `{ user, project, count}`, or `:help` if help was given.
   """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean],
+                                     aliases:  [ h: :help ])
+    case parse do
+      { [ help: true], _, _ } -> :help
+      { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
+      { _, [ user, project ], _ } -> { user, project, @default_count }
+      _ -> :help
+    end
+  end
 end
