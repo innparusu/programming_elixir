c270b174c997024c8ef48f3d067c00edf2b20606
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-09-30 00:32:41 +0900

    Add chapter16.2 exercise

--- exercise/chapter16/otp_servers_1/lib/otp_servers_1/stack_server.ex
+++ exercise/chapter16/otp_servers_1/lib/otp_servers_1/stack_server.ex
@@ -1,10 +1,14 @@
 # stack を実装したサーバーを作る
 # スタックを初期化する呼び出しではスタックの開始時の中身となるリストが渡される
-# pop インターフェースだけ実装
+
 defmodule OTPServers1.StackServer do
   use GenServer
 
   def handle_call(:pop, _from, [head|tail]) do
     { :reply, head, tail }
+  end
+
+  def handle_cast({:push, element}, stack) do
+    { :noreply, [element|stack] }
   end
 end
