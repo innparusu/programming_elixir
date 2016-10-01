24d2fbec62aa5f82ce79761a28c90fa101114899
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-10-01 22:09:57 +0900

    Add chapter17.2 exercise

--- exercise/chapter17/otp_supervisors/lib/otp_supervisors.ex
+++ exercise/chapter17/otp_supervisors/lib/otp_supervisors.ex
@@ -4,17 +4,6 @@ defmodule OtpSupervisors do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: OtpSupervisors.Worker.start_link(arg1, arg2, arg3)
-      worker(OtpSupervisors.StackServer, [[1,2,3,4]]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: OtpSupervisors.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = OtpSupervisors.Supervisor.start_link([1,2,3,4])
   end
 end
--- exercise/chapter17/otp_supervisors/lib/otp_supervisors/stack_server.ex
+++ exercise/chapter17/otp_supervisors/lib/otp_supervisors/stack_server.ex
@@ -3,18 +3,22 @@
 
 defmodule OtpSupervisors.StackServer do
   use GenServer
-  def start_link(init_stack), do: GenServer.start_link(__MODULE__, init_stack, name: __MODULE__)
+  def start_link(stash_pid), do: {:ok, _pid} = GenServer.start_link(__MODULE__, stash_pid, name: __MODULE__)
 
   def pop, do: GenServer.call __MODULE__, :pop
 
   def push(element), do: GenServer.cast __MODULE__, {:push, element}
 
-  def handle_call(:pop, _from, [head|tail]), do: { :reply, head, tail }
+  def init(stash_pid) do
+    current_stack = OtpSupervisors.Stash.get_stack stash_pid
+    {:ok, {current_stack, stash_pid} }
+  end
 
-  def handle_cast({:push, element}, stack), do: { :noreply, [element|stack] }
+  def handle_call(:pop, _from, {[head|tail], stash_pid}), do: { :reply, head, {tail, stash_pid} }
 
-  def terminate(reason, state) do
-    IO.puts("reason: #{inspect(reason)}")
-    IO.puts("state: #{inspect(state)}")
+  def handle_cast({:push, element}, {stack, stash_pid}), do: { :noreply, {[element|stack], stash_pid} }
+
+  def terminate(_reason, {current_stack, stash_pid}) do
+    OtpSupervisors.Stash.save_stack stash_pid, current_stack
   end
 end
