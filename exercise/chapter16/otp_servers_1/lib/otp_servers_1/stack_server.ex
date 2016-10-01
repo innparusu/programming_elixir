c161c0c731af9862b021ac65b8aa9ae1df304bc7
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-10-01 15:24:58 +0900

    Add chapter16.5 exercise

--- exercise/chapter16/otp_servers_1/lib/otp_servers_1/stack_server.ex
+++ exercise/chapter16/otp_servers_1/lib/otp_servers_1/stack_server.ex
@@ -3,12 +3,18 @@
 
 defmodule OTPServers1.StackServer do
   use GenServer
+  def start_link(init_stack), do: GenServer.start_link(__MODULE__, init_stack, name: __MODULE__)
 
-  def handle_call(:pop, _from, [head|tail]) do
-    { :reply, head, tail }
-  end
+  def pop, do: GenServer.call __MODULE__, :pop
 
-  def handle_cast({:push, element}, stack) do
-    { :noreply, [element|stack] }
+  def push(element), do: GenServer.cast __MODULE__, {:push, element}
+
+  def handle_call(:pop, _from, [head|tail]), do: { :reply, head, tail }
+
+  def handle_cast({:push, element}, stack), do: { :noreply, [element|stack] }
+
+  def terminate(reason, state) do
+    IO.puts("reason: #{inspect(reason)}")
+    IO.puts("state: #{inspect(state)}")
   end
 end
